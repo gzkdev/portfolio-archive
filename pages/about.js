e2ed63ba97c4be2e3c0f85ea339a508540ade94a
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-04-30 19:44:53 +0100

    added about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import AppHead from '../components/AppHead'
+import Header from '../components/Header'
+
+export default function About() {
+    return (
+        <>
+            <AppHead />
+            <Header />
+        </>
+    )
+}
