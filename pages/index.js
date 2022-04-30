bd8a0d6260eea089e3a3a1abc65e19ce330cb0ea
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-04-30 16:14:00 +0100

    updated global css file

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <AppHead />
       <h1>Hello</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In praesentium minus temporibus?</p>
     </>
 
   )
