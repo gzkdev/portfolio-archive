f9c1908b490ae3114ccf3e6fa36ab0b2d07d4431
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-04-30 22:45:35 +0100

    added projects section ui

---
+++ components/AppData.js
@@ -0,0 +1,44 @@
+export const ProjectsData = [
+    {
+        name: "Octomigo",
+        description: "Front-end web application to easilty fetch any Github user's profile without visiting the Github website.",
+        live_url: "https://octomigo.netlify.app",
+        repo_url: "https://github.com/",
+        keywords: ["React", "Github API"]
+    },
+    {
+        name: "Minotes",
+        description: "A progressive front-end web application built for users to make simple notes, edit them and delete them when they need to.",
+        live_url: "https://octomigo.netlify.app",
+        repo_url: "https://github.com/",
+        keywords: ["React", "Framer motion"]
+    },
+    {
+        name: "Marche",
+        description: "An outstanding landing page for a digital marketing agency focused on providing excellent digital services that help fuel the growth of modern businesses.",
+        live_url: "https://octomigo.netlify.app",
+        repo_url: "https://github.com/",
+        keywords: ["HTML", "CSS", "JavaScript"]
+    },
+    {
+        name: "Fiber",
+        description: "A landing page for an online template marketplace for developers who want a beautiful portfolio template to showcase their projects and apply to jobs.",
+        live_url: "https://octomigo.netlify.app",
+        repo_url: "https://github.com/",
+        keywords: ["HTML", "CSS", "JavaScript"]
+    },
+    {
+        name: "Coinmigo",
+        description: "A fronted web application built to help users track real-time cryptocurrency prices and visualize crypto prices changes using the Coingecko API.",
+        live_url: "https://octomigo.netlify.app",
+        repo_url: "https://github.com/",
+        keywords: ["React", "Coingecko API"]
+    },
+    {
+        name: "Dressr",
+        description: "A fronted web application built to help users track real-time cryptocurrency prices and visualize crypto prices changes using the Coingecko API.",
+        live_url: "https://octomigo.netlify.app",
+        repo_url: "https://github.com/",
+        keywords: ["React", "Framer motion"]
+    }
+]
--- components/ProjectsSection.jsx
+++ components/ProjectsSection.jsx
@@ -1,12 +1,38 @@
 import styles from "../styles/ProjectsSection.module.css";
 
+import { ProjectsData } from "./AppData";
+
 export default function ProjectsSection() {
   return (
     <section className={styles.projects}>
       <div className="container">
         <span className={styles.projects__title}>Projects</span>
         <div className={styles.projects__container}>
-          <div></div>
+          {ProjectsData.map((project, index) => (
+            <div key={index} className={styles.projects__item}>
+              <div>
+                <h4>{project.name}</h4>
+                <p>{project.description}</p>
+              </div>
+              <div className={styles.projects__item__footer}>
+                <a
+                  className={styles.projects__item__link}
+                  href={project.live_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View
+                </a>
+                <a
+                  href={project.repo_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Code
+                </a>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
