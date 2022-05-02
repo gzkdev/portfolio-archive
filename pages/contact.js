9a1f48108f3bd0294a92c59dddad82c1505c181b
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-02 15:08:32 +0100

    styled about page completely

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,10 @@
 import AppHead from '../components/AppHead'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-import styles from "../styles/Contact.module.css"
 import Loader from '../components/Loader'
+import styles from "../styles/Contact.module.css"
 
-export default function contact() {
+export default function Contact() {
     return (
         <>
             <Loader text="Contact" />
@@ -12,10 +12,29 @@ export default function contact() {
             <Header />
             <section>
                 <div className="container">
-                    <form>
+                    <div className={styles.contact__container}>
                         <span className={styles.contact__title}>Contact</span>
-                        <h2>Jake</h2>
-                    </form>
+                        <h2>Have a project you want to talk about? Let me know</h2>
+                        <form>
+                            <div className={styles.contact__form__box}>
+                                <label htmlFor="name">Name</label>
+                                <input type="text" name="name" id="name" />
+                            </div>
+
+                            <div className={styles.contact__form__box}>
+                                <label htmlFor="email">Email </label>
+                                <input type="email" name="email" id="email" />
+                            </div>
+
+                            <div className={styles.contact__form__box}>
+                                <label htmlFor="email">Your story </label>
+                                <textarea name="text" id="text" ></textarea>
+                            </div>
+
+                            <button type="submit" className="cta">Submit</button>
+
+                        </form>
+                    </div>
                 </div>
             </section>
             <Footer />
