da9a491b5cc45d92d75d6233957a027139ec962c
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-02 16:27:09 +0100

    created works catalog template

--- components/WorkSection.jsx
+++ components/WorkSection.jsx
@@ -4,8 +4,23 @@ export default function WorkSection() {
   return (
     <section>
       <div className="container">
-        <h2>Works</h2>
-        <p>A list of interesting stuff I&apos;ve built</p>
+        <div className={styles.work__container}>
+          <h2>Works</h2>
+          <p>A catalogue of interesting stuff I&apos;ve built</p>
+          <div className={styles.work__catalogue}>
+            <div className={styles.catalogue__item}>
+              <div className={styles.item__year}>Year</div>
+              <div className={styles.item__title}>Title</div>
+              <div className={styles.item__location} data-viewport>
+                Made at
+              </div>
+              <div className={styles.item__stack} data-viewport>
+                Built
+              </div>
+              <div className={styles.item__link}>Link</div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
