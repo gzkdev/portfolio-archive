f93c0a0bc3e2453516d75478cede428c49a2918d
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-02 15:50:20 +0100

    added works page, works section

---
+++ components/WorkSection.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/WorkSection.module.css";
+
+export default function WorkSection() {
+  return (
+    <section>
+      <div className="container">
+        <h2>Works</h2>
+        <p>A list of interesting stuff I&apos;ve built</p>
+      </div>
+    </section>
+  );
+}
---
+++ pages/work.js
@@ -0,0 +1,16 @@
+import AppHead from '../components/AppHead'
+import ContactSection from '../components/ContactSection'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+export default function Work() {
+    return (
+        <>
+            <AppHead />
+            <Header />
+
+            <ContactSection />
+            <Footer />
+        </>
+    )
+}
