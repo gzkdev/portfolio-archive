5966df16be2fff55575ba4304c2507e52e00956c
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-02 15:17:44 +0100

    made contact form a component

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,5 @@
 import AppHead from '../components/AppHead'
+import ContactForm from '../components/ContactForm'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Loader from '../components/Loader'
@@ -15,25 +16,7 @@ export default function Contact() {
                     <div className={styles.contact__container}>
                         <span className={styles.contact__title}>Contact</span>
                         <h2>Have a project you want to talk about? Let me know</h2>
-                        <form>
-                            <div className={styles.contact__form__box}>
-                                <label htmlFor="name">Name</label>
-                                <input type="text" name="name" id="name" />
-                            </div>
-
-                            <div className={styles.contact__form__box}>
-                                <label htmlFor="email">Email </label>
-                                <input type="email" name="email" id="email" />
-                            </div>
-
-                            <div className={styles.contact__form__box}>
-                                <label htmlFor="email">Your story </label>
-                                <textarea name="text" id="text" ></textarea>
-                            </div>
-
-                            <button type="submit" className="cta">Submit</button>
-
-                        </form>
+                        <ContactForm />
                     </div>
                 </div>
             </section>
