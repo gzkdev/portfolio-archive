c1e9fe60453f652c5d04fc40d01c72b707907672
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-02 16:59:50 +0100

    added work section template and styles

--- components/AppData.js
+++ components/AppData.js
@@ -9,21 +9,21 @@ export const ProjectsData = [
     {
         name: "Minotes",
         description: "A progressive front-end web application built for users to make simple notes, edit them and delete them when they need to.",
-        live_url: "https://octomigo.netlify.app",
+        live_url: "https://minotes.netlify.app",
         repo_url: "https://github.com/",
         keywords: ["React", "Framer motion"]
     },
     {
         name: "Marche",
         description: "An outstanding landing page for a digital marketing agency focused on providing excellent digital services that help fuel the growth of modern businesses.",
-        live_url: "https://octomigo.netlify.app",
+        live_url: "https://marche-agency.netlify.app",
         repo_url: "https://github.com/",
         keywords: ["HTML", "CSS", "JavaScript"]
     },
     {
         name: "Fiber",
         description: "A landing page for an online template marketplace for developers who want a beautiful portfolio template to showcase their projects and apply to jobs.",
-        live_url: "https://octomigo.netlify.app",
+        live_url: "https://fiber-ag.netlify.app",
         repo_url: "https://github.com/",
         keywords: ["HTML", "CSS", "JavaScript"]
     },
@@ -41,4 +41,49 @@ export const ProjectsData = [
         repo_url: "https://github.com/",
         keywords: ["React", "Framer motion"]
     }
+]
+
+export const ProjectsCatalogueData = [
+    {
+        year: "2021",
+        title: "Octomigo",
+        Made_at: "Personal",
+        stack: ["React", "Github API"],
+        live_url: "https://octomigo.netlify.app"
+    },
+    {
+        year: "2021",
+        title: "Minotes",
+        Made_at: "Personal",
+        stack: ["React", "Github API"],
+        live_url: "https://minotes.netlify.app"
+    },
+    {
+        year: "2021",
+        title: "Marche",
+        Made_at: "Personal",
+        stack: ["HTML", "CSS", "JavaScript"],
+        live_url: "https://marche-agency.netlify.app"
+    },
+    {
+        year: "2021",
+        title: "Fiber Landing page",
+        Made_at: "Personal",
+        stack: ["HTML", "CSS", "JavaScript"],
+        live_url: "https://fiber-ag.netlify.app"
+    },
+    {
+        year: "2021",
+        title: "Coinmigo",
+        Made_at: "Personal",
+        stack: ["React", "Coingecko API"],
+        live_url: "https://fiber-ag.netlify.app"
+    },
+    {
+        year: "2021",
+        title: "Dressr",
+        Made_at: "Personal",
+        stack: ["React", "Styled components"],
+        live_url: "https://fiber-ag.netlify.app"
+    },
 ]
--- components/WorkSection.jsx
+++ components/WorkSection.jsx
@@ -1,25 +1,40 @@
 import styles from "../styles/WorkSection.module.css";
+import { ProjectsCatalogueData } from "./AppData";
 
 export default function WorkSection() {
   return (
-    <section>
+    <section className={styles.work__section}>
       <div className="container">
-        <div className={styles.work__container}>
-          <h2>Works</h2>
-          <p>A catalogue of interesting stuff I&apos;ve built</p>
-          <div className={styles.work__catalogue}>
-            <div className={styles.catalogue__item}>
-              <div className={styles.item__year}>Year</div>
-              <div className={styles.item__title}>Title</div>
+        <h2>Works</h2>
+        <p>A catalogue of interesting stuff I&apos;ve built</p>
+        <div className={styles.work__catalogue}>
+          <div className={styles.catalogue__item}>
+            <div className={styles.item__year}>Year</div>
+            <div className={styles.item__title}>Title</div>
+            <div className={styles.item__location} data-viewport>
+              Made at
+            </div>
+            <div className={styles.item__stack} data-viewport>
+              Built
+            </div>
+            <div className={styles.item__link}>Link</div>
+          </div>
+
+          {ProjectsCatalogueData.map((work, index) => (
+            <div key={index} className={styles.catalogue__item}>
+              <div className={styles.item__year}>{work.year}</div>
+              <div className={styles.item__title}>{work.title}</div>
               <div className={styles.item__location} data-viewport>
-                Made at
+                {" "}
               </div>
               <div className={styles.item__stack} data-viewport>
-                Built
+                {" "}
               </div>
-              <div className={styles.item__link}>Link</div>
+              <div className={styles.item__link}>
+                <a href={work.live_url}> ↗</a>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
