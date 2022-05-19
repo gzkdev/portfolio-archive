2dd7b17f5e9d33c5e91a6873ef1fb29842462214
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-19 13:46:45 +0100

    updated about section structure and data

--- components/AboutSection.jsx
+++ components/AboutSection.jsx
@@ -1,4 +1,5 @@
 import styles from "../styles/About.module.css";
+import { JourneyData } from "./AppData";
 
 export default function AboutSection() {
   return (
@@ -65,272 +66,49 @@ export default function AboutSection() {
               <li>Node</li>
             </ul>
           </div>
-          {/* J */}
+
           <section className={styles.about__journey}>
-            <h2>My Journey so far...</h2>
-            <div className={styles.journey__story}>
-              <div className={styles.story__icon}>🛠</div>
-              <div className={styles.story__content}>
-                <h4 className={styles.story__title}>Engineering</h4>
-                <small className={styles.story__timeline}>2018 - present</small>
-                <p className={styles.story__text}>
-                  My journey as a software engineer started in 2018 when a close
-                  friend first introduced me to computers. He used to code in
-                  Python (a programming Language) those days and I can&apos;t
-                  forget how amazed I felt the first time I saw things move on
-                  the screen when someone in real life used the command line
-                  interface. At this point, I was seeing him as an Idol but he
-                  didn&apos;t know.
-                </p>
-              </div>
-            </div>
+            <h2>My Journey </h2>
+            {JourneyData.map((journey) => (
+              <>
+                <div className={styles.journey__story}>
+                  <div className={styles.story__icon}>{journey.icon}</div>
+                  <div className={styles.story__content}>
+                    <h4 className={styles.story__title}>{journey.title}</h4>
+                    <small className={styles.story__timeline}>
+                      {journey.timeline}
+                    </small>
 
-            <div className={styles.journey__story}>
-              <div className={styles.story__icon}>🎨</div>
-              <div className={styles.story__content}>
-                <h4 className={styles.story__title}>Design</h4>
-                <small className={styles.story__timeline}>2021 - present</small>
-                <p className={styles.story__text}>
-                  My journey as a software engineer started in 2018 when a close
-                  friend first introduced me to computers. He used to code in
-                  Python (a programming Language) those days and I can&apos;t
-                  forget how amazed I felt the first time I saw things move on
-                  the screen when someone in real life used the command line
-                  interface. At this point, I was seeing him as an Idol but he
-                  didn&apos;t know.
-                </p>
-              </div>
-            </div>
+                    <div className={styles.story__text}>
+                      {journey.story.map((story) => (
+                        <>
+                          <p>{story}</p>
+                        </>
+                      ))}
+                    </div>
 
-            <div className={styles.journey__story}>
-              <div className={styles.story__icon}>🎓</div>
-              <div className={styles.story__content}>
-                <h4 className={styles.story__title}>Academics</h4>
-                <small className={styles.story__timeline}>
-                  2021 - expected 2024
-                </small>
-                <div className={styles.story__text}>
-                  <p>
-                    I have quite an excellent academic record from childhood to
-                    the university. Currently, I&apos;m a 300 level student of
-                    Nnamdi Azikiwe University, Nigeria where I&apos;m studying
-                    Pharmacy (GPA 4.65/5).
-                  </p>
-                  <p>
-                    Going back to high school days, in the year 2017, I
-                    represented my State at the CSN (Chemical Society of
-                    Nigeria) National competition where I unfortunately came
-                    back with nothing at the final round.
-                  </p>
+                    {journey.list && (
+                      <>
+                        <div className={styles.story__list_container}>
+                          <span>{journey.list.title}</span>
+                          <ul className={styles.story__list}>
+                            {journey.list.items.map((item) => (
+                              <>
+                                <li>
+                                  <a href={item.url}>{item.title}</a>
+                                </li>
+                              </>
+                            ))}
+                          </ul>
+                        </div>
+                      </>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </div>
-
-            <div className={styles.journey__story}>
-              <div className={styles.story__icon}>🧪</div>
-              <div className={styles.story__content}>
-                <h4 className={styles.story__title}>Learning</h4>
-                <small className={styles.story__timeline}>2021 - present</small>
-                <p className={styles.story__text}>
-                  My journey as a software engineer started in 2018 when a close
-                  friend first introduced me to computers. He used to code in
-                  Python (a programming Language) those days and I can&apos;t
-                  forget how amazed I felt the first time I saw things move on
-                  the screen when someone in real life used the command line
-                  interface. At this point, I was seeing him as an Idol but he
-                  didn&apos;t know.
-                </p>
-
-                <div className={styles.story__list_container}>
-                  <span>Learning resources</span>
-                  <ul className={styles.story__list}>
-                    <li>
-                      <a href="#">Web development Full course</a>
-                    </li>
-                    <li>
-                      <a href="#">Intro to Algorithms and Data Structures</a>
-                    </li>
-                    <li>
-                      <a href="#">Algorithms and Data Structures Full course</a>
-                    </li>
-                    <li>
-                      <a href="#">Full stack react course</a>
-                    </li>
-                    <li>
-                      <a href="#">You don&apos;t know JS</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            <div className={styles.journey__story}>
-              <div className={styles.story__icon}>🎧</div>
-              <div className={styles.story__content}>
-                <h4 className={styles.story__title}>Music</h4>
-                <small className={styles.story__timeline}>2021 - present</small>
-                <p className={styles.story__text}>
-                  My journey as a software engineer started in 2018 when a close
-                  friend first introduced me to computers. He used to code in
-                  Python (a programming Language) those days and I can&apos;t
-                  forget how amazed I felt the first time I saw things move on
-                  the screen when someone in real life used the command line
-                  interface. At this point, I was seeing him as an Idol but he
-                  didn&apos;t know.
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.journey__story}>
-              <div className={styles.story__icon}>🎮</div>
-              <div className={styles.story__content}>
-                <h4 className={styles.story__title}>Extras</h4>
-                <small className={styles.story__timeline}>2021 - present</small>
-                <p className={styles.story__text}>
-                  My journey as a software engineer started in 2018 when a close
-                  friend first introduced me to computers. He used to code in
-                  Python (a programming Language) those days and I can&apos;t
-                  forget how amazed I felt the first time I saw things move on
-                  the screen when someone in real life used the command line
-                  interface. At this point, I was seeing him as an Idol but he
-                  didn&apos;t know.
-                </p>
-              </div>
-            </div>
+              </>
+            ))}
           </section>
         </div>
-        {/* <div className={styles.about__container}>
-          <span className={styles.about__title}>About me</span>
-          <h2>Get to know me</h2>
-          <div className={styles.about__story}>
-            <span className={styles.about__story__title}>Personally 💙</span>
-            <p>
-              First of all, my name is Godswill Ezihe Okechukwu (Most times my
-              nickname is Bobby). If you wonder what the {'"gzk"'} you see on
-              almost on my profiles mean, then you just need to relax I&apos;ll
-              explain it to you innabit.
-            </p>
-            <p>
-              The truth is that there isn&apos;t anything special about the{" "}
-              {'"gzk"'}, it is just a combination of the first consonants of my
-              real names (Godswill eZihe oKechukwu). You get it now? if you
-              don&apos;t get it forget about it.
-            </p>
-            <p>
-              I&apos;m in love with making and breaking things and that&apos;s
-              one the reasons I chose to become a software engineer. Naturally,
-              I&apos;m a very cool person that you&apos;d definitely want to be
-              hang out with.
-            </p>
-            <p>
-              Although I&apos;m naturally introverted, you don&apos;t dare stay
-              bored around me. It just cannot happen, mark it somewhere, Lol.
-            </p>
-          </div>
-
-          <div className={styles.about__story}>
-            <span className={styles.about__story__title}>Technically 🛠</span>
-            <p>
-              My journey as a software engineer started in 2018 when a close
-              friend first introduced me to computers. He used to code in Python
-              (a programming Language) those days and I can&apos;t forget how
-              amazed I felt the first time I saw things move on the screen when
-              someone in real life used the command line interface. At this
-              point, I was seeing him as an Idol but he didn&apos;t know.
-            </p>
-            <p>
-              I never was serious with learning how to code until 2020, it was
-              largely because I wasn&apos;t computer literate as of then and I
-              didn&apos;t have access to my own computer.
-            </p>
-            <p>
-              The year was 2020, I just started my first semester at Nnamdi
-              Azikiwe University where I was studying Pharmacy. Around February,
-              the Academic Union of Nigerian Universities went on strike and the
-              covid19 crisis followed suit which made students stay at home for
-              almost a year.
-            </p>
-            <p>
-              It was during the covid19 pandemic that I took coding seriously, I
-              taught myself HTML and CSS thoroughly and started writing little
-              JavaScript for my applications and websites. By October 2020, I
-              got my first internship at Silicon Africa LLC (a digital agency)
-              in South East. My experience at this Silicon Africa was a rich one
-              I can&apos;t stop talking about.
-            </p>
-            <p>
-              Fast forward to 2021, the long strike by the Academic Union was
-              called off and I went back to school. Around February this period,
-              I took a course on product design and interned at Utiva Inc as a
-              product designer. This experience exposed me to solid UX design
-              principles and helped me develop a better perspective towards
-              building user-focused products as a frontend engineer
-            </p>
-            <p>
-              We are now here in 2022, at this point I focus more of my time
-              learning more of React, JavaScript, Math, Computer Science, Data
-              Structures and Algorithms.
-            </p>
-            <p>
-              Also, I practice whatever I learn by building simple to medium
-              sized projects which are littered all over my Github account
-            </p>
-            <a
-              className={styles.about__story__link}
-              href="http://github.com/gzkdev"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              See my Github
-            </a>
-          </div>
-
-          <div className={styles.about__story}>
-            <span className={styles.about__story__title}>Academically 🎓</span>
-            <p>
-              I have quite an excellent academic reccord from childhood to the
-              university. Currently, I&apos;m a 300 level student of Nnamdi
-              Azikiwe University, Nigeria where I&apos;m studying Pharmacy (GPA
-              4.65/5).
-            </p>
-            <p>
-              Going back to high school days, in the year 2017, I represented my
-              State at the CSN (Chemical Society of Nigeria) National
-              competition where I unfortunately came back with nothing at the
-              final round.
-            </p>
-            <p>
-              These days, I teach my self Computer science (Harvard CS50
-              course), Math (MIT Opencourseware) and Algorithms (Youtube, Udemy
-              & Leetcode) while at school with the aim to be a better software
-              engineer
-            </p>
-          </div>
-
-          <div className={styles.about__story}>
-            <span className={styles.about__story__title}>Extras 🎮</span>
-            <p>
-              I enjoy working out at the Gym to stay fit, playing video games,
-              watching football matches, seeing movies (I started seeing movies
-              recently). Also, if you play table tennis please don&apos;t
-              declare yourself champion until you meet me.
-            </p>
-            <p>
-              If you enjoy Anime series then I consider you family 💙. This
-              world would be very boring without people like you.
-            </p>
-            <p>
-              I plan to travel to exotic locations around the globe and try
-              crazy stunts like sky diving, bunjee jumping (if I got the
-              spelling right this time 😅) , hiking, etc but my pockets are yet
-              to agree with my on my dreams.
-            </p>
-            <p>
-              Please don&apos;t forget I&apos;m open to opportunities to learn
-              and build useful software as a frontend engineer
-            </p>
-          </div>
-        </div> */}
       </div>
     </section>
   );
--- components/AppData.js
+++ components/AppData.js
@@ -87,3 +87,106 @@ export const ProjectsCatalogueData = [
         live_url: "https://fiber-ag.netlify.app"
     },
 ]
+
+export const JourneyData = [
+    {
+        id: 0,
+        icon: "🎓",
+        title: "Academics",
+        timeline: "2019 - expected 2024",
+        story: [
+            `I'm currently a Pharmacy student at Nnamdi Azikiwe University, Nigeria with a cumulative GPA of 4.65 / 5.0`,
+            `If you'd be impressed, I was the best graduating student in Chemistry in my High School.
+            `
+        ]
+    },
+    {
+        id: 1,
+        icon: "🛠",
+        title: "Engineering",
+        timeline: "2018 - present",
+        story: [
+            `My journey as a software engineer started in 2018 when a close friend first introduced me to computers. Since then, I have developed an unquenchable thirst for building software.`,
+            `Currently, I'm focused on building accessible and scalable web applications with JavaScript and other tools in the JavaScript ecosystem.  
+            `
+        ]
+    },
+    {
+        id: 2,
+        icon: "🎨",
+        title: "Design",
+        timeline: "2021 - present",
+        story: [
+            `After my internship at Silicon Africa LLC in 2020, I learned about the importance of UI/UX design in developing products.`,
+            `I was driven to explore the field of design not necessarily because I wanted to be a designer. It was because I felt it was important to learn the process of building rich user experiences.`,
+            `I have discovered that my sound background in UI/UX design has made a better frontend engineer. These days you will also find me learning about design systems and Storybook`
+        ]
+    },
+    {
+        id: 3,
+        icon: "🧪",
+        title: "Learning",
+        timeline: "Since I was born - till I die",
+        story: [
+            `Software engineering is constantly evolving. This means that as a software engineer, I'm always learning to be proficient in my craft.`,
+            `My learning these days are more focused on advanced JavaScript, Data structures, Algorithms, and Design systems. Sometimes, I do Math to stay sharp.`,
+            `Some of the resources that I have found to be useful are listed below.`
+        ],
+        list: {
+            title: "Learning Resources",
+            items: [
+                {
+                    title: "Intro to Data Structures and Algorithms",
+                    url: "https://freecodecamp.org"
+                },
+                {
+                    title: "JavaScript Fundamentals",
+                    url: "https://freecodecamp.org"
+                },
+                {
+                    title: "Intro to Web development",
+                    url: "https://freecodecamp.org"
+                }
+            ]
+        }
+    },
+    {
+        id: 4,
+        icon: "🎧",
+        title: "Music & Relaxation",
+        timeline: "Since I was born - till I die",
+        story: [
+            `Although I work hard as a software engineer, my life isn't all about coding. I try to avoid burnout most times by finding time to relax.`,
+            `Music is one of the things that I enjoy. You will find me streaming music most times. My favorites include rap, lo-fi beats, classical music, RnB and sad instrumental beats.`,
+            `if you have a similar taste in music then you may enjoy the playlists I listed below. I promise to keep them updated.`
+        ],
+        list: {
+            title: "Playlists",
+            items: [
+                {
+                    title: "Hanto beat maker",
+                    url: "https://freecodecamp.org"
+                },
+                {
+                    title: "Floowww",
+                    url: "https://freecodecamp.org"
+                },
+                {
+                    title: "Japanese Samurai Lofi",
+                    url: "https://freecodecamp.org"
+                }
+            ]
+        }
+    }, {
+        id: 5,
+        icon: "🎮",
+        title: "Extras",
+        timeline: "From birth - eternity",
+        story: [
+            `I love to stay active and strong. You will find me going to the gym, playing table tennis, video games and seeing movies (something I started doing recently)`,
+            `Waittt...I love Animes too. I love everything about them. My favorites include (Naruto, Demon slayer and Attack on titans).`,
+            `You must have a lot about me now, lol. Hope you enjoyed your experience on my portfolio.`,
+            `Arigato (thanks)`
+        ]
+    }
+]
