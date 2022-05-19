eca84bbd94084839e16ce1eba1d8e5bf3a32d0d4
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-19 14:38:07 +0100

    updated about structure and styles

--- components/AboutSection.jsx
+++ components/AboutSection.jsx
@@ -10,13 +10,14 @@ export default function AboutSection() {
           <h3 className={styles.about__name}>Godswill Ezihe</h3>
           <div className={styles.about__details}>
             <small>Frontend software engineer</small>
-            <button>Follow</button>
+            <button>
+              Follow <span>You&apos;ve been tricked! 😅</span>
+            </button>
           </div>
           <p className={styles.about__essay}>
-            I&apos;m in love with making and breaking things and that&apos;s one
-            the reasons I chose to become a software engineer. Naturally,
-            I&apos;m a very cool person that you&apos;d definitely want to be
-            hang out with.
+            First of all, I&apos;m human. I&apos;m a software engineer that
+            loves to learn, break and build stuff on the web. Curisoity killed
+            the cat but it turned me into a software engineer.
           </p>
           <div className={styles.about__list__container}>
             <span className={styles.about__title}>
@@ -30,67 +31,66 @@ export default function AboutSection() {
               ))}
             </ul>
           </div>
+        </div>
+        <section className={styles.about__journey}>
+          <h2>My skills</h2>
+          {SkillsData.map((type) => (
+            <>
+              <div className={styles.about__list__container}>
+                <span className={styles.about__title}>{type.title}</span>
+                <ul className={styles.about__opportunities}>
+                  {type.list.map((item) => (
+                    <>
+                      <li>{item}</li>
+                    </>
+                  ))}
+                </ul>
+              </div>
+            </>
+          ))}
+        </section>
 
-          <section className={styles.about__journey}>
-            <h2>My skills</h2>
-            {SkillsData.map((type) => (
-              <>
-                <div className={styles.about__list__container}>
-                  <span className={styles.about__title}>{type.title}</span>
-                  <ul className={styles.about__opportunities}>
-                    {type.list.map((item) => (
+        <section className={styles.about__journey}>
+          <h2>My Journey </h2>
+          {JourneyData.map((journey) => (
+            <>
+              <div className={styles.journey__story}>
+                <div className={styles.story__icon}>{journey.icon}</div>
+                <div className={styles.story__content}>
+                  <h4 className={styles.story__title}>{journey.title}</h4>
+                  <small className={styles.story__timeline}>
+                    {journey.timeline}
+                  </small>
+
+                  <div className={styles.story__text}>
+                    {journey.story.map((story) => (
                       <>
-                        <li>{item}</li>
+                        <p>{story}</p>
                       </>
                     ))}
-                  </ul>
-                </div>
-              </>
-            ))}
-          </section>
-
-          <section className={styles.about__journey}>
-            <h2>My Journey </h2>
-            {JourneyData.map((journey) => (
-              <>
-                <div className={styles.journey__story}>
-                  <div className={styles.story__icon}>{journey.icon}</div>
-                  <div className={styles.story__content}>
-                    <h4 className={styles.story__title}>{journey.title}</h4>
-                    <small className={styles.story__timeline}>
-                      {journey.timeline}
-                    </small>
-
-                    <div className={styles.story__text}>
-                      {journey.story.map((story) => (
-                        <>
-                          <p>{story}</p>
-                        </>
-                      ))}
-                    </div>
-
-                    {journey.list && (
-                      <>
-                        <div className={styles.story__list_container}>
-                          <span>{journey.list.title}</span>
-                          <ul className={styles.story__list}>
-                            {journey.list.items.map((item) => (
-                              <>
-                                <li>
-                                  <a href={item.url}>{item.title}</a>
-                                </li>
-                              </>
-                            ))}
-                          </ul>
-                        </div>
-                      </>
-                    )}
                   </div>
+
+                  {journey.list && (
+                    <>
+                      <div className={styles.story__list_container}>
+                        <span>{journey.list.title}</span>
+                        <ul className={styles.story__list}>
+                          {journey.list.items.map((item) => (
+                            <>
+                              <li>
+                                <a href={item.url}>{item.title}</a>
+                              </li>
+                            </>
+                          ))}
+                        </ul>
+                      </div>
+                    </>
+                  )}
                 </div>
-              </>
-            ))}
-          </section>
-        </div>
+              </div>
+            </>
+          ))}
+        </section>
       </div>
     </section>
   );
