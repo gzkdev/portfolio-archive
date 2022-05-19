ebf82cc4ca848ccdbbf8f2619bf8fb4098d3dcf3
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-19 14:44:05 +0100

    created about section header and added images

--- components/AboutSection.jsx
+++ components/AboutSection.jsx
@@ -1,37 +1,12 @@
 import styles from "../styles/About.module.css";
+import AboutHeader from "./AboutHeader";
 import { JourneyData, OpportunitiesData, SkillsData } from "./AppData";
 
 export default function AboutSection() {
   return (
     <section className={styles.about}>
       <div className="container">
-        <div className="about__intro">
-          <div className={styles.about__thumbnail}></div>
-          <h3 className={styles.about__name}>Godswill Ezihe</h3>
-          <div className={styles.about__details}>
-            <small>Frontend software engineer</small>
-            <button>
-              Follow <span>You&apos;ve been tricked! 😅</span>
-            </button>
-          </div>
-          <p className={styles.about__essay}>
-            First of all, I&apos;m human. I&apos;m a software engineer that
-            loves to learn, break and build stuff on the web. Curisoity killed
-            the cat but it turned me into a software engineer.
-          </p>
-          <div className={styles.about__list__container}>
-            <span className={styles.about__title}>
-              {OpportunitiesData.title}
-            </span>
-            <ul className={styles.about__opportunities}>
-              {OpportunitiesData.list.map((item) => (
-                <>
-                  <li>{item}</li>
-                </>
-              ))}
-            </ul>
-          </div>
-        </div>
+        <AboutHeader data={OpportunitiesData} />
         <section className={styles.about__journey}>
           <h2>My skills</h2>
           {SkillsData.map((type) => (
@@ -39,10 +14,8 @@ export default function AboutSection() {
               <div className={styles.about__list__container}>
                 <span className={styles.about__title}>{type.title}</span>
                 <ul className={styles.about__opportunities}>
-                  {type.list.map((item) => (
-                    <>
-                      <li>{item}</li>
-                    </>
+                  {type.list.map((item, index) => (
+                    <li key={index}>{item}</li>
                   ))}
                 </ul>
               </div>
