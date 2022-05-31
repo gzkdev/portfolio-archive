e11a42da71ad6949ef770101632b915f44d52a2a
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-31 15:29:33 +0100

    refactor: updated projects catalogue data

--- components/AppData.js
+++ components/AppData.js
@@ -3,28 +3,28 @@ export const ProjectsData = [
         name: "Minotes",
         description: "A progressive front-end web application built for users to make simple notes, edit them and delete them when they need to.",
         live_url: "https://minotes.netlify.app",
-        repo_url: "https://github.com/",
+        repo_url: "https://github.com/gzkdev/minote",
         keywords: ["React", "Framer motion"]
     },
     {
         name: "Octomigo",
         description: "Front-end web application to easilty fetch any Github user's profile without visiting the Github website.",
         live_url: "https://octomigo.netlify.app",
-        repo_url: "https://github.com/",
+        repo_url: "https://github.com/gzkdev/octomigo",
         keywords: ["React", "Github API"]
     },
     {
         name: "Marche",
         description: "An outstanding landing page for a digital marketing agency focused on providing excellent digital services that help fuel the growth of modern businesses.",
         live_url: "https://marche-agency.netlify.app",
-        repo_url: "https://github.com/",
+        repo_url: "https://github.com/gzkdev/marche-digital-agency-landing-page/tree/master",
         keywords: ["HTML", "CSS", "JavaScript"]
     },
     {
         name: "Fiber",
         description: "A landing page for an online template marketplace for developers who want a beautiful portfolio template to showcase their projects and apply to jobs.",
         live_url: "https://fiber-ag.netlify.app",
-        repo_url: "https://github.com/",
+        repo_url: "https://github.com/gzkdev",
         keywords: ["HTML", "CSS", "JavaScript"]
     }
 ]
